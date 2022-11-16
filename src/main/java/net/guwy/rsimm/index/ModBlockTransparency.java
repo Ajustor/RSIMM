b65ffa400f98d77dc1b135daefc4dadc51dbc8be
Guwy <onatkutorhan2004@gmail.com>
2022-11-16 20:35:06 +0200

    Initial commit

---
+++ src/main/java/net/guwy/rsimm/index/ModBlockTransparency.java
@@ -0,0 +1,7 @@
+package net.guwy.rsimm.index;
+
+public class ModBlockTransparency {
+    public static void init() {
+        //ItemBlockRenderTypes.setRenderLayer(ModBlocks.PALE_CREAM_DOOR.get(), RenderType.cutout());
+    }
+}
