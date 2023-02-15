10b1636e86143c6ea781629b0ea7582f0055fafb
Guwy <onatkutorhan2004@gmail.com>
2023-02-15 11:41:43 +0300

    + texture fixes

--- src/main/java/net/guwy/rsimm/index/ModItems.java
+++ src/main/java/net/guwy/rsimm/index/ModItems.java
@@ -26,23 +26,23 @@ public class ModItems {
 
     // Tools
     public static final RegistryObject<Item> CHEST_CUTTER = ITEMS.register("chest_cutter",
-            () -> new ChestCutterItem(new Item.Properties().tab(ModCreativeModeTabs.MAIN)));
+            () -> new ChestCutterItem(new Item.Properties().tab(ModCreativeModeTabs.MAIN).stacksTo(1)));
 
 
 
     // Materials
 
     public static final RegistryObject<Item> PALLADIUM_RING_MOLD = ITEMS.register("palladium_ring_mold",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN).stacksTo(1)));
 
     public static final RegistryObject<Item> PALLADIUM_RING = ITEMS.register("palladium_ring",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN).stacksTo(16)));
 
     public static final RegistryObject<Item> STEEL_ARMOR_PLATE = ITEMS.register("steel_armor_plate",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN).stacksTo(16)));
 
     public static final RegistryObject<Item> ROCKET = ITEMS.register("rocket",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.MAIN).stacksTo(32)));
 
 
 
